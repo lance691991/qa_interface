068da700bace0adf095e5fdb132ea1f9f94ac36d
suanqi <suanqi@gmail.com>
2021-07-28 09:22:14 +0000

    Update app/worker.py, start-worker files

--- app/worker.py
+++ app/worker.py
@@ -1,9 +1,5 @@
-from raven import Client
-
 from app.core.celery_app import celery_app
 from app.core.config import settings
-
-client_sentry = Client(settings.SENTRY_DSN)
 
 
 @celery_app.task(acks_late=True)
